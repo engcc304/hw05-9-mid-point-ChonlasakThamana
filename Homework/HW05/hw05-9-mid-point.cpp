d15fe46eca20fcc5fd82598aeb181b88a24bbd97
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-08-16 19:03:36 +0700

    5-9

--- Homework/HW05/hw05-9-mid-point.cpp
+++ Homework/HW05/hw05-9-mid-point.cpp
@@ -28,3 +28,21 @@
     Output:
         Mid Point of A and B is ( 4.5, -2.5 )
 */
+#include <stdio.h>
+
+int main() {
+    float x1, y1, x2, y2;
+    
+    printf("Input A Point :\n");
+    scanf("%f %f", &x1, &y1);
+    
+    printf("Input B Point :\n");
+    scanf("%f %f", &x2, &y2);
+    
+    float midX = (x1 + x2) / 2;
+    float midY = (y1 + y2) / 2;
+    
+    printf("Mid Point of A and B is ( %.1f, %.1f )\n", midX, midY);
+    
+    return 0;
+}
